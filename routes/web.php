b954da6d59d54f61fc02782525455f1dd6357763
Mirko Cifelli <146765212+MirkoCifelli@users.noreply.github.com>
2024-03-26 15:43:05 +0100

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Controllers
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\Admin\MainController as AdminMainController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [MainController::class, 'index'])->name('home');
+
+Route::prefix('admin')
+    ->name('admin.')
+    ->middleware('auth')
+    ->group(function () {
+
+    Route::get('/dashboard', [AdminMainController::class, 'dashboard'])->name('dashboard');
+
+});
+
+require __DIR__.'/auth.php';
