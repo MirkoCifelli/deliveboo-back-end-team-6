43fb8d3570b6419936c66edf1cc6b060eee7554f
martinaapalazzi <martinaapalazzi@gmail.com>
2024-03-27 16:05:09 +0100

    created views resources (index, show, create, edit) + created Route::resource in web.php

---
+++ resources/views/admin/dishes/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('page-title', 'Dishes Index')
+
+@section('main-content')
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="text-center text-success">
+                        Sei loggato! Sei su Dishes Index
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
