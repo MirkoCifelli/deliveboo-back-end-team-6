97948c79d4a789a28ff3a05185558bd848ac6aa5
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-03-27 21:17:56 +0100

    - FIXED dish-order migration

--- database/migrations/2024_03_27_182306_create_dish_order_table.php
+++ database/migrations/2024_03_27_182306_create_dish_order_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
                 ->onDelete('cascade');
 
             $table->primary(['dish_id', 'order_id']);
+            $table->tinyInteger('quantity');
         });
     }
 
