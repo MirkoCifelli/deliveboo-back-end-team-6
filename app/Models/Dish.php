8cebf515f61e1f41c63646188f050703bb6924b7
martinaapalazzi <martinaapalazzi@gmail.com>
2024-03-27 15:16:51 +0100

    Created orders -msrR + filled orders table + defined relationships in Order w dishes Model + moved controllers in folder Admin

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -9,11 +9,18 @@ class Dish extends Model
 {
     use HasFactory;
 
-     /*
+    /*
         Relationships
     */
+
+    // One to Many with resturants
     public function restaurants()
     {
         return $this->hasMany(Restaurant::class);
+    }
+
+    // Many to Many with orders
+    public function orders(){
+        return $this->belongsToMany(Order::class);
     }
 }
---
+++ database/migrations/2024_03_27_135021_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name', 128);
+            $table->string('customer_lastname', 128);
+            $table->string('customer_address', 128);
+            $table->string('customer_phone', 20);
+            $table->double('customer_total_price', 6,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
