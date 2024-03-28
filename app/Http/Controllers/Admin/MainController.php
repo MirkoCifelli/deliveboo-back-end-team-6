3a5bac6d68acf60b444af17518466056441e3601
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-03-28 13:48:55 +0100

    updated restaurant controller

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -12,10 +12,17 @@ class MainController extends Controller
 
     public function dashboard()
     {
+        //controlliamo se l'utente è autenticato 
+        //se si return --> dashboard
+        //se così non fosse facciamo un redirect alla route log in
+        $user=Auth::user();
 
-        
+        if($user){
+            return view('admin.dashboard');
+        }else{
+            return redirect()->route('login'); 
+        }
 
-        return view('admin.dashboard');
     }
 
 }
--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 //support
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 // Requests
 use App\Http\Requests\StoreRestaurantRequest;
@@ -29,9 +30,9 @@ class RestaurantController extends Controller
         if($user){
             $restaurant = $user->restaurant;
             if($restaurant){
-                dd($restaurant);
+                // dd($restaurant);
 
-                return view('admin.restaurants.index', compact('restaurant'));
+                return view('admin.restaurant.index', compact('restaurant'));
             }else{
                 return view('admin.dashboard');  
             }
@@ -40,12 +41,23 @@ class RestaurantController extends Controller
         }
     }
 
+        /**
+     * Display the specified resource.
+     */
+    public function show(string $slug)
+    {
+        $restaurant = Restaurant::where('slug', $slug)->firstOrFail();
+
+        return view('admin.restaurant.show', compact('restaurant'));
+    }
+
+
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        return view('admin.restaurant.create');
     }
 
     /**
@@ -53,31 +65,63 @@ class RestaurantController extends Controller
      */
     public function store(StoreRestaurantRequest $request)
     {
-        //
-    }
+        $validatedRestaurantData = $request->validated();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Restaurant $restaurant)
-    {
-        //
+        $ImgPath = null;
+
+        if (isset($validatedRestaurantData['img'])) {
+            $ImgPath = Storage::disk('public')->put('images', $validatedRestaurantData['img']);
+        }
+        //immagine di defaut nulla e apriamo uan condizione:"se nelle nostra validation c'è l'immagina allora salviamo il percorso:disco publico e put(mettila nella cartella images)"
+
+
+        $validatedRestaurantData['img'] = $ImgPath;
+        $restaurant = Restaurant::create($validatedRestaurantData);
+        // dd($validatedRestaurantData);
+
+        return redirect()->route('admin.restaurant.show', ['restaurant' => $restaurant->slug]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Restaurant $restaurant)
+    public function edit(string $slug)
     {
-        //
+        $restaurant = Restaurant::where('slug', $slug)->firstOrFail();
+        return view('admin.restaurant.edit',compact('restaurant'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateRestaurantRequest $request, Restaurant $restaurant)
+    public function update(UpdateRestaurantRequest $request, string $slug)
     {
-        //
+        $validatedRestaurantData = $request->validated();
+        $restaurant = Restaurant::where('slug', $slug)->firstOrFail();
+        $ImgPath = $restaurant->img;
+
+        if (isset($validatedRestaurantData['img'])) {
+            if($ImgPath != null){
+                Storage::disk('public')->delete($restaurant->img);
+            }
+
+            $ImgPath = Storage::disk('public')->put('images', $validatedRestaurantData['img']);
+
+            }else if (isset($validatedRestaurantData['delete_img'])) {
+                Storage::disk('public')->delete($restaurant->img);
+
+                $ImgPath = null;
+        }
+
+
+        $validatedRestaurantData['img'] = $ImgPath;
+        $validatedRestaurantData['slug'] = $slug = str()->slug($validatedRestaurantData['company_name']);
+        $restaurant->update($validatedRestaurantData);
+        // dd($validationResult);
+
+
+        return redirect()->route('admin.restaurant.index');
+
     }
 
     /**
