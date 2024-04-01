1bbbfd6c3fdb6af31a300e69f2d239d3775bd6f8
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-04-01 22:10:30 +0200

    - ADDED order "post" method route for Order; - Created Order Controller; - UPDATED Order table;

--- app/Http/Requests/StoreOrderRequest.php
+++ app/Http/Requests/StoreOrderRequest.php
@@ -11,7 +11,7 @@ class StoreOrderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class StoreOrderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'customer_name' => 'required|max:128',
+            'customer_lastname' => 'required|max:128',
+            'customer_address' => 'required|max:128',
+            'customer_phone' => 'required|max:20',
+            'customer_email' => 'required|email|unique:orders',
         ];
     }
 }
