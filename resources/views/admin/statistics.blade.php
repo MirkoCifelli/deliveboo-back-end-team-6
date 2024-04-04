9b3feb260a492954342ede1da9be5c1203f4ed16
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-04-04 13:55:42 +0200

    UPDATED charts

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -3,15 +3,21 @@
 @section('page-title', 'Home')
 
 @section('main-content')
+  <canvas id="monthlyOrderChart" width="500" height="200"></canvas>
+
+  <canvas id="yearlyOrderChart" width="500" height="200"></canvas>
+
+
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-  <canvas id="monthlyOrderChart" width="500" height="200"></canvas>
-  
+
   <script>
     let ctx = document.getElementById('monthlyOrderChart').getContext('2d');
+    let ctxYear = document.getElementById('yearlyOrderChart').getContext('2d');
 
     // Inizializza un array di totali con zeri per tutti i mesi
     let totals = Array.from({ length: 12 }, () => 0);
+    let totalsYear = Array.from({ length: 5 }, () => 0);
 
     fetch('/admin/monthly-orders-data')
       .then(response => response.json())
@@ -34,29 +40,13 @@
                 'rgba(144, 238, 144, 1)',
                 'rgba(255, 255, 153, 1)',
                 'rgba(135, 206, 250, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 192, 203, 1)',
-                'rgba(173, 216, 230, 1)',
-                'rgba(126, 192, 238, 1)',
-                'rgba(255, 215, 0, 1)',
-                'rgba(182, 102, 210, 1)',
-                'rgba(173, 216, 230, 1)',
-                'rgba(255, 182, 193, 1)',
-                'rgba(0, 255, 255, 1)'                                  
+                'rgba(255, 159, 64, 1)'                                
               ],
               borderColor: [
                 'rgba(144, 238, 144, 1)',
                 'rgba(255, 255, 153, 1)',
                 'rgba(135, 206, 250, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 192, 203, 1)',
-                'rgba(173, 216, 230, 1)',
-                'rgba(126, 192, 238, 1)',
-                'rgba(255, 215, 0, 1)',
-                'rgba(182, 102, 210, 1)',
-                'rgba(173, 216, 230, 1)',
-                'rgba(255, 182, 193, 1)',
-                'rgba(0, 255, 255, 1)'                                  
+                'rgba(255, 159, 64, 1)'                                
               ],
               borderWidth: 1
             }]
@@ -73,33 +63,23 @@
         });
       });  
 
-  </script>
-
-  <canvas id="yearlyOrderChart" width="500" height="200"></canvas>
-  
-  <script>
-    let ctx = document.getElementById('yearlyOrderChart').getContext('2d');
-
-    // Inizializza un array di totali con zeri per tutti i mesi
-    let totals = Array.from({ length: 5 }, () => 0);
-
     fetch('/admin/yearly-orders-data')
       .then(response => response.json())
       .then(data => {
         // Sovrascrivi i valori dei totali per i mesi con ordini effettivi
         data.forEach(item => {
-          totals[item.year - 1] = item.total;
+          totalsYear[item.year - 2020] = item.total;
         });
 
         let years = ['2020', '2021', '2022', '2023', '2024'];
 
-        let myChart = new Chart(ctx, {
+        let myChartYear = new Chart(ctxYear, {
           type: 'bar',
           data: {
-            labels: months,
+            labels: years,
             datasets: [{
               label: 'Total Orders',
-              data: totals,
+              data: totalsYear,
               backgroundColor:[
                 'rgba(144, 238, 144, 1)',
                 'rgba(255, 255, 153, 1)',
@@ -142,7 +122,10 @@
           }
         });
       });  
-
   </script>
+
+
+  
+  
    
 @endsection
