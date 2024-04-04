48e31b7a2447c235686dedc876d203107b471cdc
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-04-04 12:49:17 +0200

    UPDATED query

--- app/Http/Controllers/Admin/StatisticController.php
+++ app/Http/Controllers/Admin/StatisticController.php
@@ -21,20 +21,19 @@ class StatisticController extends Controller
 
     public function ordersData()
     {
-        // $orders = Order::selectRaw('SUM(id) as total, MONTH(created_at) as month')
-        //                ->groupBy('month')
-        //                ->get();
+        $orders = Order::selectRaw('MONTH(created_at) as month, COUNT(*) as total')
+                      ->groupBy('month')
+                      ->get();
 
-        // return response()->json($orders);
-        $year = 2024; // Anno di interesse
+         return response()->json($orders);
 
-        $orders = DB::table(DB::raw("(SELECT 1 as month UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 10 UNION SELECT 11 UNION SELECT 12) as months"))
-        ->leftJoin(DB::raw("(SELECT MONTH(created_at) as month, COUNT(id) as total FROM orders GROUP BY month) as order_counts"), 'months.month', '=', 'order_counts.month')
-        ->select('months.month', DB::raw('COALESCE(order_counts.total, 0) as total'))
-        ->orderBy('months.month')
-        ->get();
+        // $orders = DB::table(DB::raw("(SELECT 1 as month UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 10 UNION SELECT 11 UNION SELECT 12) as months"))
+        // ->leftJoin(DB::raw("(SELECT MONTH(created_at) as month, COUNT(id) as total FROM orders GROUP BY month) as order_counts"), 'months.month', '=', 'order_counts.month')
+        // ->select('months.month', DB::raw('COALESCE(order_counts.total, 0) as total'))
+        // ->orderBy('months.month')
+        // ->get();
                 
-            return response()->json($orders);
+        //return response()->json($orders);
 
     }
 
--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -10,68 +10,68 @@
   <script>
     let ctx = document.getElementById('orderChart').getContext('2d');
 
-      fetch('/admin/orders-data')
-        .then(response => response.json())
-        .then(data => {
-            let months = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 
-            'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
-            let totals = [];
-          // console.log(data)
+    // Inizializza un array di totali con zeri per tutti i mesi
+    let totals = Array.from({ length: 12 }, () => 0);
 
-          data.forEach(item => {
-              // months.push(item.month);
-              totals.push(item.total);
-          });
+    fetch('/admin/orders-data')
+      .then(response => response.json())
+      .then(data => {
+        // Sovrascrivi i valori dei totali per i mesi con ordini effettivi
+        data.forEach(item => {
+          totals[item.month - 1] = item.total;
+        });
 
-          let myChart = new Chart(ctx, {
-              type: 'bar',
-              data: {
-                  labels: months,
-                  datasets: [{
-                    label: 'Total Orders',
-                    data: totals,
-                    backgroundColor:[
-                                      'rgba(144, 238, 144, 1)',
-                                    'rgba(255, 255, 153, 1)',
-                                    'rgba(135, 206, 250, 1)',
-                                    'rgba(255, 159, 64, 1)',
-                                    'rgba(255, 192, 203, 1)',
-                                    'rgba(173, 216, 230, 1)',
-                                    'rgba(126, 192, 238, 1)',
-                                    'rgba(255, 215, 0, 1)',
-                                    'rgba(182, 102, 210, 1)',
-                                    'rgba(173, 216, 230, 1)',
-                                    'rgba(255, 182, 193, 1)',
-                                    'rgba(0, 255, 255, 1)'                                  
-                                  ],
-                    borderColor: [
-                                      'rgba(144, 238, 144, 1)',
-                                    'rgba(255, 255, 153, 1)',
-                                    'rgba(135, 206, 250, 1)',
-                                    'rgba(255, 159, 64, 1)',
-                                    'rgba(255, 192, 203, 1)',
-                                    'rgba(173, 216, 230, 1)',
-                                    'rgba(126, 192, 238, 1)',
-                                    'rgba(255, 215, 0, 1)',
-                                    'rgba(182, 102, 210, 1)',
-                                    'rgba(173, 216, 230, 1)',
-                                    'rgba(255, 182, 193, 1)',
-                                    'rgba(0, 255, 255, 1)'                                  
-                                  ],
-                    borderWidth: 1
-                  }]
-              },
-              options: {
-                  scales: {
-                      yAxes: [{
-                          ticks: {
-                            beginAtZero: true
-                          }
-                      }]
-                  }
-              }
-          });
+        let months = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
+
+        let myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: months,
+            datasets: [{
+              label: 'Total Orders',
+              data: totals,
+              backgroundColor:[
+                'rgba(144, 238, 144, 1)',
+                'rgba(255, 255, 153, 1)',
+                'rgba(135, 206, 250, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 192, 203, 1)',
+                'rgba(173, 216, 230, 1)',
+                'rgba(126, 192, 238, 1)',
+                'rgba(255, 215, 0, 1)',
+                'rgba(182, 102, 210, 1)',
+                'rgba(173, 216, 230, 1)',
+                'rgba(255, 182, 193, 1)',
+                'rgba(0, 255, 255, 1)'                                  
+              ],
+              borderColor: [
+                'rgba(144, 238, 144, 1)',
+                'rgba(255, 255, 153, 1)',
+                'rgba(135, 206, 250, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 192, 203, 1)',
+                'rgba(173, 216, 230, 1)',
+                'rgba(126, 192, 238, 1)',
+                'rgba(255, 215, 0, 1)',
+                'rgba(182, 102, 210, 1)',
+                'rgba(173, 216, 230, 1)',
+                'rgba(255, 182, 193, 1)',
+                'rgba(0, 255, 255, 1)'                                  
+              ],
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            }
+          }
+        });
       });  
-    </script>
+  </script>
    
 @endsection
