fc4ad175176a818eadd5daa4e94c3c70ffa1609f
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-04-08 12:52:22 +0200

    UPDATED restaurant api controller

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -12,11 +12,25 @@ class RestaurantController extends Controller
 {
     public function index(Request $request){
 
+        /*
+            Intercetto i paramentri passati dal front-end,
+            per poi racchiuderli in due variabili
+        */
         $slug = $request->input('slug');
         $typologies = $request->input('typologies');
 
+        /*
+            Per prima cosa creo una nuova instanza dell'oggetto Restaurant, 
+            portando con sè le sue relazioni con i dishes e le typologies.
+        */
         $restaurants = Restaurant::with('dishes', 'typologies');
 
+        /*
+            Tramite delle condizioni, costruisco l'instanza dell'oggetto,
+            e quindi la query, controllando se i parametri passati dal front-end sono impostati.
+            
+            In quel caso richiamo solamente i ristoranti che rispondono a quei parametri
+        */
         if(isset($slug)){
             $restaurants->where('slug', 'like', "%$slug%");
         }
@@ -27,7 +41,10 @@ class RestaurantController extends Controller
             });
         }
 
-        $results = $restaurants->get();
+        /*
+            Creo la query, impaginando con sei risultati per pagina
+        */
+        $results = $restaurants->paginate(6)->get();
 
 
         
